80cbe860dfabea34afce10a2930e4b2547e121d8
Mikhail Miroshnikov <mirrosha26@gmail.com>
2023-03-20 22:19:50 +0300

    Calculator update

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,20 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def calculator_views(request, **kwargs):
+    key = kwargs['key']
+    servings = int(request.GET.get('servings'))
+    if not servings:
+        servings = 1
+
+    recipe = DATA.get(key)
+    if recipe:
+        for key, value in recipe.items():
+            recipe[key] = value * servings
+            
+    context = {
+        'recipe': recipe,
+        }
+    return render(request, 'calculator/index.html', context)
